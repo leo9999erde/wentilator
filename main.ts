fe0bbe755e10a5c300764e050e5cdbed137b1955
leo9999erde <96633138+leo9999erde@users.noreply.github.com>
2021-12-25 08:58:20 +0100

    '24.12.2021, 22:22:46 Erste Dateien für MakeCode-Projekt ' wiederherstellen '

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 input.onPinPressed(TouchPin.P1, function () {
     basic.showLeds(`
         # . # . #
-        # # # # .
+        . # . # .
         # . . . #
         . # . # .
         # . # . #
         `)
-    for (let index = 0; index < 16; index++) {
+    for (let index = 0; index < 46666636; index++) {
         music.playMelody("A E B G D F C5 C ", 120)
     }
 })
