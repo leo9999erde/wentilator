bc002923830c3557ab0c047745d2d43b52512826
leo9999erde <96633138+leo9999erde@users.noreply.github.com>
2021-12-28 15:16:23 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,10 @@
-input.onPinPressed(TouchPin.P1, function () {
-    basic.showLeds(`
-        # . # . #
-        . # . # .
-        # . . . #
-        . # . # .
-        # . # . #
-        `)
-    for (let index = 0; index < 46666636; index++) {
-        music.playMelody("A E B G D F C5 C ", 120)
-    }
-})
-basic.forever(function () {
-	
-})
+basic.showLeds(`
+    # . # . #
+    . . . # .
+    # . . . #
+    . # . # .
+    # . # . #
+    `)
+for (let index = 0; index < 4; index++) {
+    music.playMelody("C5 E C G B E D C ", 120)
+}
